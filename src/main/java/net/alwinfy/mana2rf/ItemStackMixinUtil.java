3e76ad6ae21f7e6099a1948840848b202bf684a3
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2021-02-26 13:31:30 -0600

    Fix mana items refusing to push/pull from things
    I am top tier modder haha yes

--- src/main/java/net/alwinfy/mana2rf/ItemStackMixinUtil.java
+++ src/main/java/net/alwinfy/mana2rf/ItemStackMixinUtil.java
@@ -4,12 +4,15 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.energy.CapabilityEnergy;
 
+import vazkii.botania.api.mana.IManaItem;
+
 public final class ItemStackMixinUtil {
 	public static final String TARGET = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;";
 	public static final String CONVERT_TAG = "mana2rf:can_convert";
 	public static final Item energyItemWrapper = new FEItemWrapper();
 
 	public static Item redirGetItem(ItemStack in) {
-		return in.getCapability(CapabilityEnergy.ENERGY).isPresent() ? energyItemWrapper : in.getItem();
+		Item item = in.getItem();
+		return !(item instanceof IManaItem) && in.getCapability(CapabilityEnergy.ENERGY).isPresent() ? energyItemWrapper : item;
 	}
 }
